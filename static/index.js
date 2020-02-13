64880464327ed5f0ced6baaaab46bd56e65f98c6
Bernard Teo <btzy1996@hotmail.com>
2020-02-13 23:00:33 +0800

    Add loader and test

--- static/index.js
+++ static/index.js
@@ -1,8 +1,9 @@
-import init, { greet } from "./hello_wasm.js";
+import init from "./loader.js";
 
 async function run() {
-    await init();
-    await greet("WebAssembly");
+    let wasm = await init("test.wasm");
+    let ret = await wasm.test(10);
+    alert(ret);
 }
 
 run();
