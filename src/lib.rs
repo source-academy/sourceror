acc8c47d915b1316027008c83ff7389e84479da5
Bernard Teo <btzy1996@hotmail.com>
2020-02-13 22:09:37 +0800

    Add temporary test code to generate wasm file in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use wasm_bindgen::prelude::*;
 
 mod wasmgen;
 mod iter;
+mod searchablevec;
+
 
 #[wasm_bindgen]
 extern {
@@ -13,4 +15,29 @@ extern {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn gen() -> std::io::Result<()> {
+        use crate::wasmgen::*;
+        use std::fs::File;
+        use std::io::prelude::*;
+        let mut module = WasmModule::default();
+        let functype = FuncType::new(Box::new([ValType::I32]), Box::new([ValType::I32]));
+        let mut code_builder = CodeBuilder::new(functype);
+        {
+            let (locals_builder, expr_builder) = code_builder.split();
+            expr_builder.local_get(locals_builder.param(0)); // put the 0th parameter onto the stack
+            expr_builder.end(); // return
+        }
+        let (_type_idx, func_idx) = module.add_func(code_builder);
+        module.export_func(func_idx, "test".to_string());
+        let mut file = File::create("test.wasm")?;
+        let mut receiver = std::vec::Vec::<u8>::new();
+        module.wasm_serialize(&mut receiver);
+        file.write_all(receiver.as_slice())?;
+        Ok(())
+    }
 }
