dacb54d491fc1d4991236d071783b52a7ad123b8
Bernard Teo <btzy1996@hotmail.com>
2020-02-23 17:50:19 +0800

    Add pre-generated functions for number in IR

--- source-compiler/src/ir/mod.rs
+++ source-compiler/src/ir/mod.rs
@@ -1,13 +1,16 @@
 /**
  * The structs here represent the intermediate representation of the program.
  * In particular:
- * * All variables are represented with the location it should be read/written to (i.e. local#1, or param#1.var#1, or param#1.var#1.var#1, or global#1).
+ * * All variables are represented with the location it should be read/written to (i.e. local#1.var#1, or local#1.var#1.var#1, or global#1).
+ * * Note: A param is implicitly a local (with the same index space), and param indices preceed the local indices.
+ * * Note: Params may also be modified (just like locals and globals).  They do not affect the value in the caller (because of pass-by-value).
  * * All expressions have optional type annotation (which will be `any` for now).
  * * All functions are top level and don't have closures (the closure is just treated as another parameter).
  * * Top level code are put in the entry point function.
  * *
  * * There are no constant/variable declarations - semantic analyser should hoist all declarations:
  * * Semantic analyser should generate a struct to put all 'local variables' into, and place this struct in Func::locals.  Same for global variables, put them in a single struct that is in Program::globals.
+ * * If params need to be captured, semantic analyser should copy them into the struct too.
  * * IR optimisation passes might pull out fields and put them in separate local variables (can create a new struct type so we don't disturb the old one, then dead code elimination can remove the old struct).
  * * There is no difference between constant declarations and variable declarations in the IR.
  * *
@@ -20,13 +23,17 @@ use std::default::Default;
 
 mod primfunc;
 
+
+pub type FuncIdx = usize;
+
 pub struct Program {
     struct_types: Vec<Box<[VarType]>>, // stores the list of fields of all structs (i.e. objects) in the program
     funcs: Vec<Func>, // list of functions (some will be pre-generated for the pre-declared operators, e.g. + - * / % === and more)
     globals: Vec<VarType>, // list of global variables
-    entry_point: u32, // index of function to run when the program is started
+    entry_point: FuncIdx, // index of function to run when the program is started
 }
 
+#[derive(Copy, Clone)]
 pub enum VarType {
     Any, // used if we don't know the type contained in the variable.  Most of the time we will use this.  Generates a variant in the output program unless it gets optimised away.
     Undefined,
@@ -34,7 +41,7 @@ pub enum VarType {
     Boolean,
     String, // reference type
     Func, // holds a function ptr and a closure
-    StructT{typeidx: u32}, // reference type; typeid starts from zero and should be in range [0, object_types.len()).
+    StructT{typeidx: usize}, // reference type; typeid starts from zero and should be in range [0, object_types.len()).
 }
 impl Default for VarType {
     fn default() -> Self {
@@ -42,14 +49,14 @@ impl Default for VarType {
 	}
 }
 
-type Block = Vec<Statement>;
+pub type Block = Vec<Statement>;
 
 pub struct Func {
-    params: Vec<VarType>, // list of function parameters
-    result: VarType,
+    params: Box<[VarType]>, // list of function parameters
+    result: Option<VarType>, // if `None`, it means that this function never returns (e.g. it guarantees to trap, see the generated runtime error function)
     locals: Vec<VarType>, // list of local variables
     statements: Block, // body of the function
-    signature_filter: Vec<(Vec<VarType>, VarType, u32)>, // list of possibly acceptable signatures (param_types, return_type, constrained_func).
+    signature_filter: Vec<(Box<[VarType]>, VarType, FuncIdx)>, // list of possibly acceptable signatures (param_types, return_type, constrained_func).
                                                         // If a signature is not in this list, then it will be guaranteed to error;
                                                         // but converse need not be true.  All entries must be a subtype of `params`.
                                                         // `constrained_func` (funcidx) is a version of this function that has the specified param_types and return_type of this entry.
@@ -62,6 +69,7 @@ pub struct Func {
 pub enum Statement {
     Assign{target: TargetExpr, expr: Expr}, // Assigment statement
     Return{expr: Expr}, // Return statmenent,
+    // todo!: Should If be an Expr instead of a separate statement?  (Wasm if-statements can return a value.)
     If{cond: Expr, true_stmts: Block, false_stmts: Block}, // If statement
     Expr{expr: Expr}, // Expression statement
 }
@@ -74,13 +82,13 @@ pub struct Expr {
 // Represents any lvalue (assignable value)
 pub enum TargetExpr {
     // if the field is a struct, then `next` can (but not necessarily) refer to a field inside the struct
-    Global{globalidx: u32, next: Option<Box<StructField>>}, // for targetting a global variable
-    Local{localidx: u32, next: Option<Box<StructField>>}, // for targetting a local variable
+    Global{globalidx: usize, next: Option<Box<StructField>>}, // for targetting a global variable
+    Local{localidx: usize, next: Option<Box<StructField>>}, // for targetting a local variable
 }
 
 pub struct StructField {
-    typeidx: u32, // the struct type id (index into struct_types)
-    fieldidx: u32, // the index of the referred field in the struct
+    typeidx: usize, // the struct type id (index into struct_types)
+    fieldidx: usize, // the index of the referred field in the struct
     next: Option<Box<StructField>>, // if the field is a struct, then this can (but not necessarily) refer to a field inside the struct
 }
 
@@ -89,16 +97,18 @@ pub enum ExprKind {
     PrimNumber{val: f64}, // e.g. `2`
     PrimBoolean{val: bool}, // e.g. `true`
     PrimString{val: String}, // e.g. `"hello world"`, may be placed in a region of memory immune to garbage collection
-    PrimFunc{funcidx: u32}, // e.g. `() => {}`
-    VarName{source: TargetExpr}, // something that is located somewhere in memory (i.e. an lvalue), e.g. `x`
-    PrimAppl{prim_func: PrimFunc, args: Box<[Expr]>}, // primitive operations (e.g. number+number) hardcoded into the compiler.  Expr must have the correct VarType.  Should not be added directly by semantic analyser, because parser is not type-aware.
+    PrimInst{funcidx: FuncIdx}, // e.g. `() => {}`
+    TypeAssert{expr: Box<Expr>, vartype: VarType}, // returns undefined if the expr has correct type, otherwise traps.  Generated by compiler.
+    VarName{source: TargetExpr}, // something that is located somewhere in memory (i.e. an lvalue), e.g. `x`.  Note: even if `x` has type `any`, the Expr::vartype of this expr can still be more specific, if the compiler can prove its correctness.  i.e. we can load from a variant without checking the tag, if we already proved that the data must be of a particular VarType.
+    PrimAppl{prim_inst: PrimInst, args: Box<[Expr]>}, // primitive operations (e.g. number+number) hardcoded into the compiler.  Expr must have the correct VarType.  Should not be added directly by semantic analyser, because parser is not type-aware.
     Appl{func: Box<Expr>, args: Box<[Expr]>}, // function application (operators are functions too)
+    DirectAppl{funcidx: FuncIdx, args: Box<[Expr]>}, // direct function application (operators are functions too)
 }
 
 // private enum of possible primitive functions, used by pre-declared operators, or added during type-checking optimisation
 // these functions expect a particular type signature
 // this is an implementation detail of the IR, and subject to change
-enum PrimFunc {
+enum PrimInst {
     NumberAdd,
     NumberSub,
     NumberMul,
@@ -117,18 +127,59 @@ enum PrimFunc {
     BooleanNot,
     NumberNegate,
     StringAdd,
+    Trap, // used for runtime errors (e.g. type errors).  args should currently be empty, but subject to change.
 }
 
+// enum of pre-declared operators
+#[derive(Eq, PartialEq, Copy, Clone)]
+#[repr(u8)]
+pub enum Builtin {
+    Plus,
+    Minus,
+    Times,
+    Divide,
+    Modulo,
+    Eq,
+    Neq,
+    Gt,
+    Lt,
+    Ge,
+    Le,
+    And,
+    Or,
+    Not,
+    UnaryMinus,
+}
+pub const NUM_BUILTINS : u8 = Builtin::UnaryMinus as u8 + 1;
+
 impl Program {
-    pub fn new() -> Program {
-        // populates `funcs` with pre-declared operators, then returns the Program
-        todo!("populate 'funcs' with pre-declared operators");
-        todo!("populate some table, or have some static table to map the operator to the funcidx");
-        Program{
-            struct_types: Default::default(),
-            funcs: Default::default(),
-            globals: Default::default(),
-            entry_point: Default::default(),
+    // Creates an empty Program, and an array of pre-declared operators that can be used.
+    // `funcs` will have pre-declared operators, but might also have other primitive functions (e.g. typed version of pre-declared operators).
+    // The caller should only use the functions that match the funcidxs specified in the returned array of pre-declared operators.
+    // Other things in the `funcs` array should not be used.
+    pub fn new() -> (Program, [FuncIdx; NUM_BUILTINS as usize]) {
+        let (funcs, builtin_funcidxs) = primfunc::make_pregenerated_funcs();
+        (
+            Program{
+                struct_types: Default::default(),
+                funcs: funcs,
+                globals: Default::default(),
+                entry_point: Default::default(),
+		    },
+            builtin_funcidxs
+        )
+	}
+}
+
+impl Func {
+    pub fn new_with_params_and_result(params: &[VarType], result: VarType) -> Func
+    {
+        Func{
+            params: params.into(),
+            result: Some(result),
+            locals: Default::default(),
+            statements: Default::default(),
+            signature_filter: Default::default(), 
 		}
 	}
 }
---
+++ source-compiler/src/ir/primfunc.rs
@@ -0,0 +1,161 @@
+/**
+ * This module contains code to generate the primitive functions (e.g. `+(string, string) -> string`) and pre-declared operators (e.g. `+(any, any) -> any`).
+ * The pre-declared operators internally call the primitive functions
+ */
+ 
+use std::vec::Vec;
+use super::*;
+
+fn indexed_push<T>(v: &mut Vec<T>, value: T) -> usize
+{
+    let ret = v.len();
+    v.push(value);
+    ret
+}
+
+/**
+ * Generates the primitive function (e.g. `+(number, number) -> number`) with the given primitive instruction.
+ */
+fn generate_number_binary_operator_primitive(prim_inst: PrimInst) -> Func
+{
+    // create new Func with params and result
+    let mut func = Func::new_with_params_and_result(&[VarType::Number, VarType::Number], VarType::Number);
+
+    // set the statements to just the PrimAppl with the given PrimInst
+    // Equivalent code:
+    // return local#0 + local#1;
+    func.statements.push(
+        Statement::Return{
+            expr: Expr{
+                vartype: VarType::Number,
+                kind: ExprKind::PrimAppl{prim_inst: prim_inst, args:
+                    Box::new([
+                        Expr{
+                            vartype: VarType::Number,
+                            kind: ExprKind::VarName{
+                                source: TargetExpr::Local{localidx: 0, next: None}
+                                }
+                            },
+                        Expr{
+                            vartype: VarType::Number,
+                            kind: ExprKind::VarName{
+                                source: TargetExpr::Local{localidx: 1, next: None}
+                                }
+                            }
+                        ])
+                    }
+                }
+            }
+        );
+
+    // note: since this is a primitive, it has no locals and no signature_filter (because nothing is more specialized than this function)
+
+    // return the function
+    func
+}
+
+/**
+ * Generates the builtin (e.g. `+(any, any) -> any`) that asserts the argument types then calls the given primitive function.
+ * `prim_funcidx` should be called if the types are acceptable.
+ * Otherwise, it will execute Builtin::Trap.
+ */
+fn generate_number_binary_operator_wrapper(prim_funcidx: FuncIdx) -> Func
+{
+    // create new Func with params and result
+    // Note: we set the 'number' return type (because if this function ever returns, it must return a number)
+    let mut func = Func::new_with_params_and_result(&[VarType::Any, VarType::Any], VarType::Number);
+
+    // set the statements to check the type
+    // Equivalent code:
+    // typeassert(local#0, NUMBER);
+    // typeassert(local#1, NUMBER);
+    // return `prim_funcidx`(local#0, local#1);
+    func.statements.push(
+        Statement::Expr{
+            expr: Expr{
+                vartype: VarType::Undefined,
+                kind: ExprKind::TypeAssert{expr: Box::new(
+                    Expr{
+                        vartype: VarType::Any,
+                        kind: ExprKind::VarName{
+                            source: TargetExpr::Local{localidx: 0, next: None}
+                            }
+                        }
+				    ),
+                    vartype: VarType::Number},
+			    }  
+		    }
+        );
+    func.statements.push(
+        Statement::Expr{
+            expr: Expr{
+                vartype: VarType::Undefined,
+                kind: ExprKind::TypeAssert{expr: Box::new(
+                    Expr{
+                        vartype: VarType::Any,
+                        kind: ExprKind::VarName{
+                            source: TargetExpr::Local{localidx: 1, next: None}
+                            }
+                        }
+				    ),
+                    vartype: VarType::Number},
+			    }  
+		    }
+        );
+    func.statements.push(
+        Statement::Return{
+            expr: Expr{
+                vartype: VarType::Number,
+                kind: ExprKind::DirectAppl{
+                    funcidx: prim_funcidx,
+                    args: Box::new([
+                        Expr{
+                            vartype: VarType::Number,
+                            kind: ExprKind::VarName{
+                                source: TargetExpr::Local{localidx: 0, next: None}
+                                }
+                            },
+                        Expr{
+                            vartype: VarType::Number,
+                            kind: ExprKind::VarName{
+                                source: TargetExpr::Local{localidx: 1, next: None}
+                                }
+                            }
+                        ])
+                    },
+			    }  
+		    }
+        );
+
+    // note: no locals
+
+    // signature_filter for the primitive version:
+    func.signature_filter.push((Box::new([VarType::Number, VarType::Number]), VarType::Number, prim_funcidx));
+
+    // return the function
+    func
+}
+
+/**
+ * Generates the primitive function (e.g. `+(number, number) -> number`) and the builtin (e.g. `+(any, any) -> any`).
+ */
+fn generate_number_binary_operator(funcs: &mut Vec<Func>, builtin_funcidxs: &mut [FuncIdx; NUM_BUILTINS as usize], prim_inst: PrimInst, builtin: Builtin)
+{
+    let primitive_funcidx = indexed_push(funcs, generate_number_binary_operator_primitive(prim_inst));
+    let builtin_funcidx = indexed_push(funcs, generate_number_binary_operator_wrapper(primitive_funcidx));
+    builtin_funcidxs[builtin as usize] = builtin_funcidx;
+}
+
+pub fn make_pregenerated_funcs() -> (Vec<Func>, [FuncIdx; NUM_BUILTINS as usize])
+{
+    let mut funcs = Vec::<Func>::new();
+    let mut builtin_funcidxs: [FuncIdx; NUM_BUILTINS as usize] = Default::default();
+    
+    generate_number_binary_operator(&mut funcs, &mut builtin_funcidxs, PrimInst::NumberAdd, Builtin::Plus); // TODO: we need to change this to allow StringAdd for Plus
+    generate_number_binary_operator(&mut funcs, &mut builtin_funcidxs, PrimInst::NumberSub, Builtin::Minus);
+    generate_number_binary_operator(&mut funcs, &mut builtin_funcidxs, PrimInst::NumberMul, Builtin::Times);
+    generate_number_binary_operator(&mut funcs, &mut builtin_funcidxs, PrimInst::NumberDiv, Builtin::Divide);
+    generate_number_binary_operator(&mut funcs, &mut builtin_funcidxs, PrimInst::NumberRem, Builtin::Modulo);
+    
+    (funcs, builtin_funcidxs)
+}
