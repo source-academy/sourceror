636d18e4e90dbfb66c934a30f6a06a7224481785
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-03-11 20:13:29 +0800

    Add initial structure and extract relevant section of JSON

--- lib-asttoir/src/lib.rs
+++ lib-asttoir/src/lib.rs
@@ -1,22 +1,77 @@
 use std::env;
 use std::fs;
+use serde::{Deserialize, Serialize};
+
+/**
+ * The structs functions serve as utility functions to extract information from the AST
+ * into a suitable Intermediate Representation.
+ * In particular:
+ * * Entry points are zero-indexed. Main function will be #0
+ * * For now, we will leave everything as type Any
+ * Sample function that we will be decoding
+```
+function f() {
+const x = 4;
+const y = x * x;
+const z = y + 5;
+return z;
+}
+```
+* *
+* *
+* *
+ */
 static FILENAME: &str = "ast.txt";
 
-pub fn hello() {
-    println!("Hello, world!");
-}
-
 pub fn ReadFromFile() -> std::string::String {
-    // Read in Sample
     println!("In file {}", FILENAME);
-
     let contents = fs::read_to_string(FILENAME).expect("Something went wrong reading the file");
-
-    println!("With text:\n");
+    let json_contents: serde_json::Value = serde_json::from_str(&contents).unwrap();
+    // TODO: [Joel] (Find Semantic meaning for these constants)
+    let program_code_excl_imports = &json_contents["body"][11]["expression"]["callee"]["body"]["body"][0]["argument"]["callee"]["body"]["body"][0]["body"][58];
+    println!("User entered code section is {}", program_code_excl_imports["body"][0]["body"]);
     return contents;
 }
 
-// Program::new()
+pub fn PopulateFuncs() {
+    todo!();
+}
+
+pub fn PopulateFunc() {
+    PopulateParams::PopulateFuncParams();
+    PopulateFuncResult::PopulateFuncResult();
+    PopulateFuncLocals::PopulateFuncLocals();
+    PopulateFuncStatements::PopulateFuncStatements();
+    todo!();
+}
+mod PopulateParams {
+    pub fn PopulateFuncParams() {
+        // Using the JSON Passed in we want to populate the Func struct
+        todo!();
+    }
+}
+
+mod PopulateFuncResult {
+    pub fn PopulateFuncResult() {
+        // Using the struct passed in we want to populate the struct
+        todo!();
+    }
+}
+
+mod PopulateFuncLocals {
+    pub fn PopulateFuncLocals() {
+        // Populate the local variables based on struct passed in
+        todo!();
+    }
+
+}
+
+mod PopulateFuncStatements {
+    pub fn PopulateFuncStatements() {
+        // Populate Vector of statements
+        todo!();
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- source-compiler/src/main.rs
+++ source-compiler/src/main.rs
@@ -3,23 +3,10 @@ use ir;
 use serde::{Deserialize, Serialize};
 use std::vec::Vec;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
 fn main() {
-    let point = Point { x: 1, y: 2 };
     let contents = asttoir::ReadFromFile();
-    // Convert the Point to a JSON string.
-    // let serialized = serde_json::to_string(&point).unwrap();
     let a = ir::Func::new_with_params_and_result(&[ir::VarType::Any], ir::VarType::Any);
-
     let json_contents: serde_json::Value = serde_json::from_str(&contents).unwrap();
-
-    println!("contents are : {:?}", json_contents);
-    println!("Contents are : {}\n", contents);
-
-    // Prints deserialized = Point { x: 1, y: 2 }
+    // This is how to get the code
+    // println!("Contents are {}", json_contents["body"][11]["expression"]["callee"]["body"]["body"][0]["argument"]["callee"]["body"]["body"][0]["body"][58]["body"][0]["body"]);
 }
