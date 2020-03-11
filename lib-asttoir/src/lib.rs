9c34c9ff4274dbf5dcc1c602bb10f6c81d1d4059
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-03-11 23:29:34 +0800

    Add test structure

--- lib-asttoir/src/lib.rs
+++ lib-asttoir/src/lib.rs
@@ -21,62 +21,96 @@ return z;
 * *
 * *
  */
-static FILENAME: &str = "ast.txt";
 
-pub fn ReadFromFile() -> std::string::String {
-    println!("In file {}", FILENAME);
-    let contents = fs::read_to_string(FILENAME).expect("Something went wrong reading the file");
+pub fn read_from_file(filename: Option<std::string::String>) -> std::string::String {
+    let default_filename = "ast.txt".to_string();
+    let contents = std::fs::read_to_string(filename.unwrap_or(default_filename)).expect("Something went wrong reading the file");
     let json_contents: serde_json::Value = serde_json::from_str(&contents).unwrap();
     // TODO: [Joel] (Find Semantic meaning for these constants)
-    let program_code_excl_imports = &json_contents["body"][11]["expression"]["callee"]["body"]["body"][0]["argument"]["callee"]["body"]["body"][0]["body"][58];
-    println!("User entered code section is {}", program_code_excl_imports["body"][0]["body"]);
+    let program_code_excluding_imports = &json_contents["body"][11]["expression"]["callee"]["body"]["body"][0]["argument"]["callee"]["body"]["body"][0]["body"][58];
+    println!("User entered code section is {}", program_code_excluding_imports["body"][0]["body"][0]);
     return contents;
 }
 
-pub fn PopulateFuncs() {
+pub fn populate_funcs() {
+    // Read from file, extract
     todo!();
 }
 
-pub fn PopulateFunc() {
-    PopulateParams::PopulateFuncParams();
-    PopulateFuncResult::PopulateFuncResult();
-    PopulateFuncLocals::PopulateFuncLocals();
-    PopulateFuncStatements::PopulateFuncStatements();
+
+pub fn populate_func() {
+    populate_func_params();
+    populate_func_result();
+    populate_func_locals();
+    populate_func_statements();
     todo!();
 }
-mod PopulateParams {
-    pub fn PopulateFuncParams() {
-        // Using the JSON Passed in we want to populate the Func struct
-        todo!();
-    }
+
+pub fn populate_func_params() {
+        // Look through array one. Since function is global, it should not need 
+        // to be filled 
+    todo!();
 }
 
-mod PopulateFuncResult {
-    pub fn PopulateFuncResult() {
-        // Using the struct passed in we want to populate the struct
-        todo!();
-    }
+
+pub fn populate_func_result() {
+    // Check the last index of the array to get the result.
 }
 
-mod PopulateFuncLocals {
-    pub fn PopulateFuncLocals() {
-        // Populate the local variables based on struct passed in
-        todo!();
-    }
 
+pub fn populate_func_locals() {
+    // Go to body, loop through declarations to  get all variables
+    // Check index 1 of the code section, 
+    todo!();
 }
 
-mod PopulateFuncStatements {
-    pub fn PopulateFuncStatements() {
-        // Populate Vector of statements
-        todo!();
-    }
+pub fn populate_func_statements() {
+    // Statements can be If, Expr, return
+    todo!();
 }
+
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_can_read_default_file() {
+        let ast = read_from_file(None);
+        assert_eq!(2 + 3, 4);
+    }
+
+    #[test]
+    fn it_can_populate_funcs() {
+        let ast = read_from_file(None);
+        todo!();
+    }
+    #[test]
+    fn it_can_populate_locals() {
+        let ast = read_from_file(None);
+        todo!();
+        // assert_eq!(2 + 3, 4);
+    }
+
+    #[test]
+    fn it_can_populate_result() {
+        let ast = read_from_file(None);
+        todo!();
+    }
+
+    #[test]
+    fn it_can_populate_func_params() {
+        let ast = read_from_file(None);
+        todo!();
+    }
+
+    #[test]
+    fn it_can_populate_func_statements() {
+        let ast = read_from_file(None);
+        todo!();
     }
 }
--- source-compiler/src/main.rs
+++ source-compiler/src/main.rs
@@ -4,9 +4,10 @@ use serde::{Deserialize, Serialize};
 use std::vec::Vec;
 
 fn main() {
-    let contents = asttoir::ReadFromFile();
+    let contents = asttoir::read_from_file(None);
     let a = ir::Func::new_with_params_and_result(&[ir::VarType::Any], ir::VarType::Any);
     let json_contents: serde_json::Value = serde_json::from_str(&contents).unwrap();
     // This is how to get the code
+    // println!("contents are {}", json_contents);
     // println!("Contents are {}", json_contents["body"][11]["expression"]["callee"]["body"]["body"][0]["argument"]["callee"]["body"]["body"][0]["body"][58]["body"][0]["body"]);
 }
