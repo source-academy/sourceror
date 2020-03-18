8b19cc8c8039435194fde5e180bf4d394b65be9e
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-03-18 14:30:58 +0800

    Add populate_func

--- lib-asttoir/src/lib.rs
+++ lib-asttoir/src/lib.rs
@@ -19,11 +19,10 @@ return z;
 ```
 * *
 * *
-* *
  */
 
 pub fn read_from_file(filename: Option<std::string::String>) -> serde_json::Value {
-    let default_filename = "../ast.txt".to_string();
+    let default_filename = "./ast.txt".to_string();
     let contents = std::fs::read_to_string(filename.unwrap_or(default_filename))
         .expect("Something went wrong reading the file");
     let json_contents: serde_json::Value = serde_json::from_str(&contents).unwrap();
@@ -33,48 +32,73 @@ pub fn read_from_file(filename: Option<std::string::String>) -> serde_json::Valu
         .clone();
     println!(
         "User entered code section is {}",
-        program_code_excluding_imports["body"][0]["body"][0]
+        program_code_excluding_imports["body"][0]["body"][2]
     );
+    populate_func_statements(program_code_excluding_imports["body"][0]["body"].clone());
+
     return program_code_excluding_imports;
 }
 
 pub fn populate_funcs() {
-    // Read from file, extract
     todo!();
 }
 
-pub fn populate_func() {
-    todo!();
+pub fn populate_func(ast: serde_json::Value) -> ir::Func {
+    let func: ir::Func = ir::Func {
+        params: populate_func_params(ast.clone()),
+        result: populate_func_result(ast.clone()),
+        locals: populate_func_locals(ast.clone()),
+        statements: populate_func_statements(ast.clone()),
+        signature_filter: vec![],
+    };
+    return func;
 }
 
 pub fn populate_func_params(ast: serde_json::Value) -> Box<[ir::VarType]> {
-    // Look through array one. Since function is global, it should not need
-    todo!();
+    // TODO [Joel] (Complete the logic for this function)
+    return Box::new([]);
 }
 
 pub fn populate_func_result(ast: serde_json::Value) -> Option<ir::VarType> {
-    // Check the last index of the array to get the result.
+    let unwrapped_arr = ast.as_array().unwrap();
+    for line in unwrapped_arr {
+        println!("This value is {}", line);
+    }
     return Some(ir::VarType::Any);
 }
 
 pub fn populate_func_locals(ast: serde_json::Value) -> Vec<ir::VarType> {
-    // Go to body, loop through declarations to  get all variables
-    let func_local = vec![ir::VarType::Any];
+    // Loop through the array, look for "arguments" field to get declarations
+    let unwrapped_arr = ast.as_array().unwrap();
+    let mut func_local = Vec::<ir::VarType>::new();
+    for line in unwrapped_arr {
+        if line.get("declarations") != None {
+            println!(
+                "The value is {}",
+                line.get("declarations").unwrap()[0]["id"]["name"]
+            );
+            func_local.push(ir::VarType::Any);
+        }
+    }
+
     return func_local;
 }
 
 pub fn populate_func_statements(ast: serde_json::Value) -> ir::Block {
-    // Statements can be If, Expr, return
-    todo!();
+    // Statements are either Assign, Return, If, Expr, Void
+    // Sequentially classify each type of expression based on the node type
+    let unwrapped_arr = ast.as_array().unwrap();
+    let func_statements = Vec::<ir::Statement>::new();
+
+    for line in unwrapped_arr {
+        println!("func_statement is :{}", line);
+    }
+    return func_statements;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    fn setup() {
-        let ast = read_from_file(None);
-    }
 
     #[test]
     fn it_can_read_default_file() {
@@ -89,29 +113,31 @@ mod tests {
         let ast = read_from_file(None);
         todo!();
     }
+
     #[test]
     fn it_can_populate_locals() {
         let ast = read_from_file(None);
-        todo!();
+        let expected_params = populate_func_params(ast);
+        // assert_eq!(expected_params, Box::new([ir::VarType::Number, ir::VarType::Number,ir::VarType::Number]));
     }
 
     #[test]
     fn it_can_populate_result() {
-        tests::setup();
         let ast = read_from_file(None);
         let expected_locals = populate_func_result(ast);
+
         assert_eq!(expected_locals, Some(ir::VarType::Any));
     }
 
     #[test]
     fn it_can_populate_func_params() {
-        tests::setup();
-        todo!();
+        let ast = read_from_file(None);
+        let expected_params = populate_func_params(ast);
+        // assert_eq!(expected_params, Box::new([ir::VarType::Any, ir::VarType::Any]));
     }
 
     #[test]
     fn it_can_populate_func_statements() {
-        tests::setup();
         todo!();
     }
 }
