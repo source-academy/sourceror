d7b6b7a678f432d1fd9ed070e86b6958ef4923b7
Bernard Teo <btzy1996@hotmail.com>
2020-03-20 01:15:27 +0800

    Add linked list test case for Cheney

--- wasm-test-driver/src/lib.rs
+++ wasm-test-driver/src/lib.rs
@@ -5,12 +5,10 @@ use wasmgen::*;
 
 use wasm_test_harness::*;
 
-
 #[wasm_bindgen(js_namespace = test, js_name = add_test)]
 extern "C" {
     fn add_test(wasm_binary: Box<[u8]>);
 }
-
 
 /*
 Returns a valid wasm binary that should be executed by invoking "main" function.
