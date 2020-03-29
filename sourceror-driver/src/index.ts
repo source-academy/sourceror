68701c4e6ea0f27f9ff10017b520959b06daec72
Bernard Teo <btzy1996@hotmail.com>
2020-03-29 16:57:00 +0800

    Add package

---
+++ sourceror-driver/src/index.ts
@@ -0,0 +1,22 @@
+class CompileError {
+    desc: string;
+    constructor(desc: string) {
+        this.desc = desc;
+    }
+}
+
+class RuntimeError {
+    desc: string;
+    constructor(desc: string) {
+        this.desc = desc;
+    }
+}
+
+export async function compile(code: string, options: any) -> Promise<WebAssembly.Module> {
+    return {};
+}
+
+
+export async function run(wasm_module: WebAssembly.Module) {
+    return new RuntimeError("test");
+}
