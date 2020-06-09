9b27be8bf0753724417eada61dea400455f8832a
angelsl <angelsl@in04.sg>
2020-06-10 01:32:52 +0800

    Include all files in wasm folder

--- sourceror-driver/postbuild.js
+++ sourceror-driver/postbuild.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 // fix the wasm package.json so Node accepts it
 const packageJson = JSON.parse(fs.readFileSync("wasm/package.json", "utf8"));
 packageJson.type = "module";
+packageJson.files = ["*"];
 fs.writeFileSync("wasm/package.json", JSON.stringify(packageJson, undefined, 2));
 
 // copy load-wasm.js to dist
