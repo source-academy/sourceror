2879937a36815d25be0f5386b297794f35b664fb
Bernard Teo <btzy1996@hotmail.com>
2020-08-02 18:22:42 +0800

    Fix builtin and entry-point funcidx when there are imports, and empty line in FFI import support, and make import errors stop retry fetch

--- sourceror-driver/src/platform.ts
+++ sourceror-driver/src/platform.ts
@@ -9,7 +9,9 @@
  */
 export function makePlatformImports() {
   return {
+    // MATH library
     math: {
+      // trigonometric functions
       sin: Math.sin,
       cos: Math.cos,
       tan: Math.tan,
@@ -17,12 +19,39 @@ export function makePlatformImports() {
       acos: Math.acos,
       atan: Math.atan,
       atan2: Math.atan2,
+      // hyperbolic functions
       sinh: Math.sinh,
       cosh: Math.cosh,
       tanh: Math.tanh,
       asinh: Math.asinh,
       acosh: Math.acosh,
       atanh: Math.atanh,
-    }
-  }
+      // exponent/logarithm functions
+      sqrt: Math.sqrt,
+      cbrt: Math.cbrt,
+      exp: Math.exp,
+      expm1: Math.expm1,
+      log: Math.log,
+      log1p: Math.log1p,
+      log2: Math.log2,
+      log10: Math.log10,
+      pow: Math.pow,
+      // hypot function
+      hypot: Math.hypot,
+      // rounding functions
+      ceil: Math.ceil,
+      floor: Math.floor,
+      round: Math.round,
+      trunc: Math.trunc,
+      fround: Math.fround,
+      // integer operations
+      clz32: Math.clz32,
+      imul: Math.imul,
+      // random function
+      random: Math.random,
+    },
+    misc: {
+      get_time: new Date().getTime(),
+    },
+  };
 }
