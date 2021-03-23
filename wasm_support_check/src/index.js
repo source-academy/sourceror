56cbf8aa10cde9e2e337ed6b6621231cdfa35565
yzia2000 <e0381439@u.nus.edu>
2021-03-23 16:27:07 +0800

    change for node

--- wasm_support_check/src/index.js
+++ wasm_support_check/src/index.js
@@ -1,5 +1,7 @@
-import * as check from 'wasm-check';
+// import * as check from 'wasm-check';
+const check = require('wasm-check');
 
 console.log(`WASM Support: ${check.support()}`);
+console.log(`WASM 2 Support: ${check.support(2)}`);
 console.log(`Features:`);
 console.log({...check.feature});
