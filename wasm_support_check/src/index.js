8f57270c6aacea7a8196a569076d165c09213d99
yzia2000 <e0381439@u.nus.edu>
2021-03-24 12:37:06 +0800

    removed WASM 2 Features check from wasm_check

--- wasm_support_check/src/index.js
+++ wasm_support_check/src/index.js
@@ -2,6 +2,5 @@
 const check = require('wasm-check');
 
 console.log(`WASM Support: ${check.support()}`);
-console.log(`WASM 2 Support: ${check.support(2)}`);
 console.log(`Features:`);
 console.log({...check.feature});
