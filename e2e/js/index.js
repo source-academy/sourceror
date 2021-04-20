ffd069621a02514db3b6261686653ccc69902058
yzia2000 <e0381439@u.nus.edu>
2021-04-21 02:02:03 +0800

    added support for import in e2e

--- e2e/js/index.js
+++ e2e/js/index.js
@@ -2,31 +2,10 @@ import Harness from './harness';
 
 const harness = new Harness;
 const code = `
-function abs(x) {
-  return x >= 0 ? x : 0 - x;
+import {math_sin, math_PI} from "https://btzy.github.io/libsourceror/std/math.source";
+function x(n) {
+  return n === 0 ? 0 : ((t) => { return x(n - t); })(1);
 }
-function square(x) {
-  return x * x;
-}
-function average(x,y) {
-  return (x + y) / 2;
-}
-function sqrt(x) {
-  function good_enough(guess, x) {
-    return abs(square(guess) - x) < 0.001;
-  }
-  function improve(guess, x) {
-    return average(guess, x / guess);
-  }
-  function sqrt_iter(guess, x) {
-    return good_enough(guess, x)
-      ? guess
-      : sqrt_iter(improve(
-        guess, x), x);
-  }
-  return sqrt_iter(1.0, x);
-}
-
-sqrt(10011014123122145131234);  
+x(3);
 `;
-harness.runCode(code, 100055055460.09229);
+harness.runCode(code, 1);
