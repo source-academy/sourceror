f690a5d377d99e3e4e6a47cfdb7f10fe456dbb5b
yzia2000 <e0381439@u.nus.edu>
2021-04-21 03:28:06 +0800

    tested imports in e2e testing

--- e2e/js/index.js
+++ e2e/js/index.js
@@ -3,9 +3,10 @@ import Harness from './harness';
 const harness = new Harness;
 const code = `
 import {math_sin, math_PI} from "https://btzy.github.io/libsourceror/std/math.source";
+
 function x(n) {
-  return n === 0 ? 0 : ((t) => { return x(n - t); })(1);
+  return n === 0 ? 0 : math_sin(n);
 }
-x(1000000);
+x(30);
 `;
 harness.runCode(code, 1);
