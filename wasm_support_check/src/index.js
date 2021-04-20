7a5e5f38bd9e6b78268ef586b5ddd09a33c98e63
Mohammad Yousuf Minhaj Zia <42372397+yzia2000@users.noreply.github.com>
2021-04-20 13:19:52 +0800

    Revert "Feat: Browser support check for WASM"

--- wasm_support_check/src/index.js
+++
@@ -1,6 +0,0 @@
-// import * as check from 'wasm-check';
-const check = require('wasm-check');
-
-console.log(`WASM Support: ${check.support()}`);
-console.log(`Features:`);
-console.log({...check.feature});
